00f6beefe003d9d845708254d244eb4cc989dac6
Dongsheng Cai <imdongsheng@gmail.com>
2009-02-10 01:36:24 +0800

    Split definition string by \x00

--- type_stardict.py
+++ type_stardict.py
@@ -1,28 +1,36 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import string
+import re
+import gzip
+from struct import *
 
 class dict(object):
     def __init__(self, options):
+        print 'From StarDict'
+        print '============'
         name = options['dict_data']
         files = {
-            'inf_file': name + '/' + name + '.ifo',
-            'idx_file': name + '/' + name + '.idx',
-            'dic_file': name + '/' + name + '.dic'
-        }
+                'inf_file': name + '/' + name + '.ifo',
+                'idx_file': name + '/' + name + '.idx',
+                'dic_file': name + '/' + name + '.dict'
+                }
         self.engine = stardict_engine(files)
     def get_result(self, word):
-		self.result = {}
-		self.result['word'] = word
-		self.result['pron'] = 'sdfj'
-		self.result['def']  = ['test']
-		return self.result
+        self.result = {}
+        cords = self.engine.search_idx(word)
+        data  = self.engine.search_word(cords)
+        test  = data.split('\x00')
+        self.result['word'] = word
+        self.result['pron'] = test[0]
+        del test[0]
+        self.result['def']  = test
+        return self.result
 
 class stardict_engine(object):
     def __init__(self, options):
         self.options = options
         self.setup_info()
-        self.search_idx('test')
     def setup_info(self):
         f = self.options['inf_file']
         f = open(f, 'r')
@@ -34,10 +42,37 @@ class stardict_engine(object):
             tmp[1].strip()
             self.dict_info[str(tmp[0])] = str(tmp[1].strip())
         return self.dict_info
-    def search_idx(self, word):
+    def search_word(self, cords):
+        try:
+            self.dic_file = open(self.options['dic_file'], 'rb')
+        except IOError:
+            try:
+                self.dic_file = gzip.open(self.options['dic_file'] + '.dz', 'rb')
+            except IOError:
+                print 'wrong'
+        self.dic_file.seek(cords[0])
+        bytes = self.dic_file.read(cords[1])
+        return bytes
+
+    def search_idx(self, search_word):
         f = open(self.options['idx_file'], 'rb')
-        f.read()
-        print f.tell()
+        self.idx_file = f.read()
+        # offset 
+        if self.dict_info.has_key('idxoffsetbits'):
+            idx_offset_bytes_size = self.dict_info['idxoffsetbits']/8
+        else:
+            idx_offset_bytes_size = 4
+        idx_format = {4: 'L', 8: 'Q'}[idx_offset_bytes_size]
+        # word data size
+        idx_offset_bytes_size = idx_offset_bytes_size + 4
+        record_regex = r'([\d\D]+?\x00[\d\D]{%s})' % idx_offset_bytes_size
+        matched_records = re.findall(record_regex, self.idx_file)
 
-
-
+        word_list = {}
+        for record in matched_records:
+            c = record.find('\x00') + 1
+            record_tuple = unpack('!%sc%sL' % (c, idx_format), record)
+            word  = record_tuple[0:c-1]
+            cords = record_tuple[c:]
+            word_list[''.join(word)] = cords
+        return word_list[search_word]
